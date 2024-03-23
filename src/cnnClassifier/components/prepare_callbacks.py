1a29ac243cddbe9fea83d879288e231dc6ad19fa
rushisanganii <rushi.sangani@yahoo.com>
2024-03-23 12:26:45 -0400

    Prepare callbacks added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -4,6 +4,7 @@ from zipfile import ZipFile
 import tensorflow as tf
 import time
 from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
+from keras.callbacks import TensorBoard
 
 
 class PrepareCallback:
